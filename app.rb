6cf9c9eae39f32bf3bc05f42da1d88a328c018cf
reneew08 <rwu30@chicagobooth.edu>
2024-10-23 02:31:56 +0000

    completed

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get('/rock') do
+  @user_choice='rock'
+  @choices=['rock','paper','scissors']
+  @computer_choice=@choices.sample
+  erb(:game)
+end
+
+get('/paper') do
+  @user_choice='paper'
+  @choices=['rock','paper','scissors']
+  @computer_choice=@choices.sample
+  erb(:game)
+end
+
+get('/scissors') do
+  @user_choice='scissors'
+  @choices=['rock','paper','scissors']
+  @computer_choice=@choices.sample
+  erb(:game)
 end
